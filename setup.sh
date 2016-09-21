995fb13bfc885121639c391c8107b613841e4057
me388 <me388@cam.ac.uk>
2016-09-21 16:26:09 +0100

    Some more dotfiles and a crude setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s .vimrc $HOME/.vimrc
+ln -s .config/base16-harmonic16-dark.sh $HOME/.config/base16-harmonic16-dark.sh
+ln -s .zshrc $HOME/.zshrc
+ln -s .i3rc $HOME/.i3/config
+ln -s .sshrc $HOME/.ssh/config
+ln -s .config/flake8 $HOME/.config/flake8
