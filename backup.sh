66f2c8d772cc238e07282f77bb6ca4b0a9c00122
ml-evs <me388@cam.ac.uk>
2016-12-13 20:18:48 +0000

    Random updates

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
-rsync -av -e --delete ssh ~/documents noggin:~/
-rsync -av --exclude '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj ~/
+rsync -av --delete -e ssh ~/documents noggin:~/
+rsync -av --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj ~/
