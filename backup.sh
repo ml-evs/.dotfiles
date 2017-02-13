a0b69fa224483311fc619fd00d410295fe3fb9a2
ml-evs <me388@cam.ac.uk>
2017-02-13 12:07:48 +0000

    Better backup to noggin

--- backup.sh
+++ backup.sh
@@ -1,2 +1,3 @@
 rsync -av --delete -e ssh ~/documents noggin:~/
 rsync -av --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj ~/
+rsync -av --delete --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj/LiPX ~/proj/
