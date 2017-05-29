e8125b14454875ebaf8e76a9a8464de438ec05d4
ml-evs <me388@cam.ac.uk>
2017-05-29 18:20:55 +0100

    Trying to fix rsync

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
-rsync -av --delete -e ssh ~/documents noggin:~/
-rsync -av --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj ~/
-rsync -av --delete --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e ssh noggin:~/proj/LiPX ~/proj/
+rsync -av --delete -e "ssh -i /home/matthew/.ssh/id_rsa" /home/matthew/documents me388@cluster2.tcm.phy.cam.ac.uk:~/
+rsync -av --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e "ssh -i /home/matthew/.ssh/id_rsa" me388@cluster2.tcm.phy.cam.ac.uk:~/proj /home/matthew
+rsync -av --delete --exclude-from '/home/matthew/.dotfiles/rsync_exclude.txt' -e "ssh -i /home/matthew/.ssh/id_rsa" me388@cluster2.tcm.phy.cam.ac.uk:~/proj/LiPX /home/matthew/proj/
