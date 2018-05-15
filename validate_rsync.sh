e43961b3d62f71c5333fdafac620dfff11c36280
ml-evs <me388@cam.ac.uk>
2018-05-15 14:56:02 +0100

    Updated rsync validation script

--- validate_rsync.sh
+++ validate_rsync.sh
@@ -1,34 +1,10 @@
 #!/bin/sh
 # from https://www.linux.com/news/making-secure-remote-backups-rsync
 case "$SSH_ORIGINAL_COMMAND" in 
-*\&*) 
-echo "Rejected1" 
-;; 
-*\(*) 
-echo "Rejected2" 
-;; 
-*\{*) 
-echo "Rejected3" 
-;; 
-*\;*) 
-echo "Rejected5" 
-;; 
-*\<*) 
-echo "Rejected6" 
-;; 
-*\>*) 
-echo "Rejected7" 
-;; 
-*\`*) 
-echo "Rejected8" 
-;; 
-*\|*) 
-echo "Rejected9" 
-;; 
 rsync\ --server*) 
 $SSH_ORIGINAL_COMMAND 
 ;; 
 *) 
-echo "Rejected10" 
+echo "Key rejected: this ssh key can only call rsync" 
 ;; 
 esac 
