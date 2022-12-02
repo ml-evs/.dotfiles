76e97a212c52a39f73fe31cbda35e8c551cf4e29
Matthew Evans <git@ml-evs.science>
2022-12-02 18:31:06 +0000

    Make push/pull from tw optional

--- scripts/task-git.sh
+++ scripts/task-git.sh
@@ -10,11 +10,20 @@ if [ ! -d "$DATA_DIR" ]; then
   exit 1
 fi
 
+PULL=0
+for i
+do
+  if [ "$i" == "--pull" ]; then
+    PULL=1
+    shift
+  fi
+done
+
 # Check if --task-git-push is passed as an argument.
 PUSH=0
 for i
 do
-  if [ "$i" == "--task-git-push" ]; then
+  if [ "$i" == "--push" ]; then
     # Set the PUSH flag, and remove this from the arguments list.
     PUSH=1
     shift
@@ -22,8 +31,13 @@ do
 done
 
 # Call task, commit files and push if flag is set.
-/usr/bin/task $@
+
 cd $DATA_DIR
+if [ "$PULL" == 1 ]; then
+  git pull > /dev/null
+fi
+
+/usr/bin/task $@
 git add .
 git commit -m "$TASK_COMMAND" > /dev/null
 
