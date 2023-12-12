8ed2a6d0ccdfbe65dc9b61e6063d2c3c8e3e9c55
Matthew Evans <git@ml-evs.science>
2023-12-12 23:34:26 +0000

    Add polybar launch

---
+++ .i3/polybar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 0.1; done
+
+polybar -r mine &
+polybar -r laptop &
+polybar -r vert &
+polybar -r ext1 &
+polybar -r ext2
+
+# for m in $(polybar -m | awk '{gsub(":", "", $1); print $1}'); do
+#     MONITOR=$m polybar -r mine &
+# done
