9dfcb1cabc18aa6349bafdf36657e698147b87b9
Matthew Evans <git@ml-evs.science>
2024-07-12 13:03:36 +0100

    Disable notif sound

--- .config/dunst/sound.sh
+++ .config/dunst/sound.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ ! "$DUNST_APP_NAME" = "tidal-hifi" ]; then
     echo $DUNST_APP_NAME
-    paplay /home/mevans/.config/dunst/pda.mp3
+    #paplay /home/mevans/.config/dunst/pda.mp3
 fi
